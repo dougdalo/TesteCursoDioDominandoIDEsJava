bac20e2809a05a6b6bb493335b19f502e7ddc6b0
dougdalo <dougdalo@gmail.com>
2021-08-21 16:09:31 -0300

    Commit Calculadora #1

--- src/br/com/dio/JavaProject.java
+++ src/br/com/dio/JavaProject.java
@@ -11,11 +11,7 @@ public class JavaProject {
 		
 		System.out.println(gato);
 		System.out.println(livros);
-		/*int a=2;
-		int b=3;
 		
-		System.out.println(" Hello World " + ( a + b ));
-		*/
 		
 		
 	}
